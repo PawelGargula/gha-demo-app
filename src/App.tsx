de2cae377c0b146d2442ffa9252af55ba6996653
Paweł Gargula <pawel.gargula@interia.pl>
2024-11-16 13:34:34 +0100

    Add tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { increase, decrease } from './utils/counterTransformer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,18 +17,21 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Counter: {count}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button 
+          type='button' 
+          onClick={() => setCount((count) => increase(count))}
+        >
+          Increase
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button 
+          type='button' 
+          onClick={() => setCount((count) => decrease(count))}
+        >
+          Decrease
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
