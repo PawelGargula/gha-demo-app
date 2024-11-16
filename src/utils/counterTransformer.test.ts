533a039bf868152fb59341225674e7f16ec4d44f
Paweł Gargula <pawel.gargula@interia.pl>
2024-11-16 13:41:12 +0100

    Update tests

--- src/utils/counterTransformer.test.ts
+++ src/utils/counterTransformer.test.ts
@@ -4,12 +4,12 @@ import { increase, decrease } from "./counterTransformer";
 describe("counterTransformer", () => {
     test("should increase value", () => {
         expect(increase(1)).toBe(2);
-        expect(increase(6)).toBe(8);
+        expect(increase(5)).toBe(7);
         expect(increase(12)).toBe(15);
     });
 
     test("should decrease value", () => {
-        (expect(decrease(4)).toBe(3));
+        (expect(decrease(5)).toBe(3));
         (expect(decrease(8)).toBe(6));
         (expect(decrease(21)).toBe(18));
     });
