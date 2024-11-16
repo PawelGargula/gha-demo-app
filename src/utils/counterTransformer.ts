51220581c8f5b86d6c1d5977e6ac5baf6acb2b51
Paweł Gargula <pawel.gargula@interia.pl>
2024-11-16 13:43:42 +0100

    Wrong update of increase function

--- src/utils/counterTransformer.ts
+++ src/utils/counterTransformer.ts
@@ -1,7 +1,7 @@
 export function increase(count: number): number {
     if (count > 10) {
         return count + 3;
-    } else if (count >= 5) {
+    } else if (count > 5) {
         return count + 2;
     } else {
         return count + 1;
